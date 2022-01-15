b787cce1bf0b224a5aa2ab3f3800d11e3889dbf4
Lorenzo <LorenzG@users.noreply.github.com>
2022-01-15 17:54:00 +0100

    update

--- utils.py
+++ utils.py
@@ -1,6 +1,14 @@
 from collections.abc import Iterable
 
+import pandas as pd
+import numpy as np
+import cudf
+import cupy
+
+
 def coerce_iterable(obj):
+    if type(obj) in [pd.DataFrame, pd.Series, cudf.DataFrame, cudf.DataFrame, np.array, cupy.array]:
+        return [obj]
     if not isinstance(obj, Iterable):
         return [obj]
     return obj
